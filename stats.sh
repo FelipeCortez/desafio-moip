6dad2ef4c7fe9a11679f02fc3d9448388e739558
Felipe Cortez <felipecortezfi@gmail.com>
2018-06-18 12:56:09 -0300

    Solução comentada

--- stats.sh
+++ stats.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
+# cut separa pelas aspas e pega a segunda parte
+# sort uniq -c sort -nr conta linhas repetidas e ordena pelas mais frequentes
+# awk troca as colunas para deixar no formato pedido na especificação
 order_by_freq() {
     cut -d '"' -f2 | sort | uniq -c | sort -nr | awk '{print $2 " - " $1}'
 }
 
+# o primeiro GREP seleciona matches para request_to="..."
+# head -n 3 seleciona as três primeiras linhas
 grep --text -o -E 'request_to=\"[^\"]*\"' $1 | order_by_freq | head -n 3;
 echo;
 grep --text -o -E 'response_status=\"\d+\"' $1 | order_by_freq
