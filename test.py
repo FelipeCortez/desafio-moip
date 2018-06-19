38ab0dd9ecf899d23ffd7aaa7b113fa7afad0187
Felipe Cortez <felipecortezfi@gmail.com>
2018-06-19 12:31:32 -0300

    Teste unitário para caixa alta e baixa

--- test.py
+++ test.py
@@ -1,14 +1,21 @@
 import unittest
 from stats import parse_file
 
+def load_file(filename: str):
+    with open(filename, "rb") as expected:
+        return expected.read().decode("utf-8")
+
 class TestParse(unittest.TestCase):
 
     def test_well_formed(self):
         parse_contents = parse_file("testes/well-formed.txt")
+        expected_contents = load_file("testes/well-formed.expected")
+        self.assertEqual(parse_contents, expected_contents)
 
-        with open("testes/well-formed.expected", "rb") as expected:
-            expected_contents = expected.read().decode("utf-8")
-            self.assertEqual(parse_contents, expected_contents)
+    def test_mixed(self):
+        parse_contents = parse_file("testes/mixedcase.txt")
+        expected_contents = load_file("testes/mixedcase.expected")
+        self.assertEqual(parse_contents, expected_contents)
 
     def test_malformed(self):
         with self.assertRaises(ValueError):
