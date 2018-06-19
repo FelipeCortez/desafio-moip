9e91f79a02d566fd19ef70d4d7aa949e1f6acfcc
Felipe Cortez <felipecortezfi@gmail.com>
2018-06-19 10:05:55 -0300

    Mais tratamento de erros

--- stats.py
+++ stats.py
@@ -11,29 +11,33 @@ if __name__ == "__main__":
         print("Please specify the log file")
         sys.exit()
 
-    with open(filename, 'rb') as f:
-        count_url = Counter()
-        count_status = Counter()
+    try:
+        with open(filename, 'rb') as f:
+            count_url = Counter()
+            count_status = Counter()
 
-        url_list = []
-        status_list = []
+            url_list = []
+            status_list = []
 
-        for l in f:
-            # tem uns caracteres especiais no começo do arquivo de log
-            line = l.decode('utf-8', errors="ignore")
+            for l in f:
+                # tem uns caracteres especiais no começo do arquivo de log
+                line = l.decode('utf-8', errors="ignore")
 
-            url = re.search('request_to=\"([^\"]*)\"', line)
-            if url: url_list.append(url.group(1).lower())
+                url = re.search('request_to=\"([^\"]*)\"', line)
+                if url: url_list.append(url.group(1).lower())
 
-            status = re.search('response_status=\"([^\"]*)\"', line)
-            if status: status_list.append(status.group(1))
+                status = re.search('response_status=\"([^\"]*)\"', line)
+                if status: status_list.append(status.group(1))
 
-        counter = Counter(url_list).most_common(3)
-        for item in sorted(counter, key=by_count_then_lex):
-            print(f"{item[0]} - {item[1]}")
+            counter = Counter(url_list).most_common(3)
+            for item in sorted(counter, key=by_count_then_lex):
+                print(f"{item[0]} - {item[1]}")
 
-        print()
+            print()
 
-        counter = Counter(status_list).most_common()
-        for item in sorted(counter, key=by_count_then_lex):
-            print(f"{item[0]} - {item[1]}")
+            counter = Counter(status_list).most_common()
+            for item in sorted(counter, key=by_count_then_lex):
+                print(f"{item[0]} - {item[1]}")
+
+    except OSError:
+        print("File not found")
--- stats.sh
+++ stats.sh
@@ -3,15 +3,21 @@
 # cut separa pelas aspas e -f2 pega a segunda parte
 # tr deixa tudo em minúsculas
 # sort uniq -c sort -nr conta linhas repetidas e ordena pelas mais repetidas
+# -k1nr e -k2 ordena primeiro pela quantidade depois lexicograficamente
 # awk troca as colunas para deixar no formato solicitado na especificação
 order_by_freq() {
     cut -d '"' -f2 | tr '[A-Z]' '[a-z]' | sort | uniq -c | sort -k1nr -k2 | awk '{print $2 " - " $1}'
 }
 
+if [ $# -eq 0 ]; then
+    echo "Please specify the log file"
+    exit 1
+fi
+
 # não continuar se não encontrar arquivo
 set -o pipefail
 # o primeiro GREP seleciona casamentos para request_to="..."
 # head -n 3 seleciona as três primeiras linhas do resultado
-grep --text -o -E 'request_to=\"[^\"]*\"' $1 | order_by_freq | head -n 3 &&
-echo &&
-grep --text -o -E 'response_status=\"\d+\"' $1 | order_by_freq
+grep --text -s -o -E 'request_to=\"[^\"]*\"' $1 | order_by_freq | head -n 3 &&
+echo || echo "File not found"
+grep --text -s -o -E 'response_status=\"\d+\"' $1 | order_by_freq
